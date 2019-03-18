ccb731dbcbd108903321c40363e87b97c1fa1c56
Justin McCormick <me@justinmccormick.com>
2019-03-17 21:50:55 -0400

    Changing to port 4000

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import WebSocket from 'ws';
 import Session from "./session"
 
-const wss = new WebSocket.Server({ port: 3006 });
+const wss = new WebSocket.Server({ port: 4000 });
 
 wss.on('connection', (socket) => {
   new Session(socket, 'primaldarkness.com', '5006');
-})
+});
